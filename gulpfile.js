014a9fdacfe8bc8d211f5a40ca0bcd950e2115d4
avmaktslave <4tharraofdagon@gmail.com>
2017-04-04 21:22:33 +0300

    add Browser Sync and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
     return gulp.src('app/sass/style.sass')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server:{
+      baseDir: 'app'
+    },
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+  gulp.watch('app/sass/style.sass',['sass']);
 });
