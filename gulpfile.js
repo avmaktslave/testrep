041b610a5b5a5cbfb2adc247de4bd0a6bc2092e3
avmaktslave <4tharraofdagon@gmail.com>
2017-04-04 21:58:00 +0300

    add task scripts for bower libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs');
 
 gulp.task('sass', function() {
     return gulp.src('app/sass/style.sass')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
+  ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('browser-sync', function() {
@@ -19,4 +31,6 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
   gulp.watch('app/sass/style.sass',['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
 });
